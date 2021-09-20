13613a3b5749e7ff077aa69f7847a264776aaace
IraTereshchenko <tereshchenkoim@gmail.com>
2021-09-20 15:55:28 +0300

    error correction task-01.js
    error correction task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,16 @@
-const categoriesRef = document.querySelectorAll('.item')
+const categoriesRef = document.querySelectorAll(".item")
+// console.log(categoriesRef.length );
 console.log(`В списке ${categoriesRef.length} категории.`);
 
 categoriesRef.forEach(elements => {
-const title = elements.querySelector('h2')
-console.log(title.textContent);
-const quantity = elements.querySelectorAll('li')
-console.log(quantity.length);
+    const title = elements.querySelector('h2')
+    // console.log(title.textContent);
+    console.log(`Категория: ${title.textContent}`);
+    const quantity = elements.querySelectorAll("li")
+    // console.log(quantity.length);
+    console.log(`Количество элементов:${quantity.length}`);
 })
+
+
+
 
