b18e63aeb55742511bb18d6eb96cc6e807433e69
IraTereshchenko <tereshchenkoim@gmail.com>
2021-09-20 15:56:07 +0300

    task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,23 @@
+const refs = {
+    decrement: document.querySelector('[data-action="decrement"]'),
+    increment: document.querySelector('[data-action="increment"]'),
+    valueRef: document.querySelector("#value")
+}
+
+refs.increment.addEventListener("click", () => {
+    let elem = Number.parseInt(refs.valueRef.textContent)
+    elem += 1
+    refs.valueRef.textContent = elem
+    console.log(refs.valueRef.textContent);
+    console.log(elem);
+})
+
+    refs.decrement.addEventListener("click", () => {
+    let elem = Number.parseInt(refs.valueRef.textContent)
+    elem -= 1
+    refs.valueRef.textContent = elem
+    // console.log(refs.valueRef.textContent);
+    console.log(elem);
+})
+
+
