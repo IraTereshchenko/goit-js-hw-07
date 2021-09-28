bd0a33124c486165edfca3339be1a275ad9f2957
IraTereshchenko <tereshchenkoim@gmail.com>
2021-09-28 20:19:58 +0300

    Delete console.log

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,10 @@
 const categoriesRef = document.querySelectorAll(".item")
-// console.log(categoriesRef.length );
 console.log(`В списке ${categoriesRef.length} категории.`);
 
 categoriesRef.forEach(elements => {
     const title = elements.querySelector('h2')
-    // console.log(title.textContent);
     console.log(`Категория: ${title.textContent}`);
     const quantity = elements.querySelectorAll("li")
-    // console.log(quantity.length);
     console.log(`Количество элементов:${quantity.length}`);
 })
 
